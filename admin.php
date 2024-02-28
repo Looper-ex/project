65ac271fcd37dfbd9315305e9391914c1d759a69
Looperex <154706146+Looper-ex@users.noreply.github.com>
2024-02-28 18:55:11 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -12,7 +12,7 @@ if(empty($_SESSION["giris"]))
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/html">
 <head>
 <title>Emlak</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
